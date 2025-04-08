8ab62b741ecb64b0da1e9468b3e34d0102f06a74
solankiRitish <140726419+solankiRitish@users.noreply.github.com>
2025-04-08 09:00:08 +0530

    solve the error

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -41,7 +41,7 @@ function Login() {
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
-          <Link to={"/Farmer.jsx"}>
+          <Link to={"/dashboard/Farmer"}>
             <button
               type="submit"
               className="w-full px-4 py-2 text-white bg-green-600 rounded-lg"
